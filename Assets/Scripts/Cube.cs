adf43817c0dcb7fd4013408c760802a6ed7cd58e
FirstTimeHere <97403208+FirstTimeHere@users.noreply.github.com>
2024-04-30 23:25:18 +0500

    fixCollision
    теперь работает корректно

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,18 +1,62 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Platform))]
 
 public class Cube : MonoBehaviour
 {
+    private Platform _platform;
+
     private bool _isColorChanged;
+
+    private float _lifeTimer;
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_lifeTimer <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        //LifeTimer?.Invoke(RandomTime);
-        if (_isColorChanged == false)
+        if (collision.gameObject == _platform.gameObject)
         {
-            _isColorChanged = true;
-            gameObject.GetComponent<MeshRenderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            if (_isColorChanged == false)
+            {
+                _isColorChanged = true;
+                gameObject.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            }
+
+            StartCoroutine(GetLifeTime(_lifeTimer));
         }
+    }
+    private IEnumerator GetLifeTime(float delay)
+    {
+        var wait = new WaitForSecondsRealtime(delay);
+
+        while (_lifeTimer > 0)
+        {
+            _lifeTimer--;
+
+            yield return wait;
+        }
+    }
+
+    public void ChangeLifeTimer(int time)
+    {
+        _lifeTimer = time;
+    }
+
+    public void GetPlatform(Platform plane)
+    {
+        _platform = plane;
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,31 +6,64 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private List<Transform> _spawnPoints;
     [SerializeField] private Cube _cube;
+    [SerializeField] private Platform _platform;
 
     [SerializeField] private float _waitTime = 0.1f;
+    [SerializeField, Range(0, 100)] private int _numbersOfCubes;
+
+    private List<Cube> _cubes = new List<Cube>();
 
     private void Start()
     {
+        CreateCubes(_numbersOfCubes);
         StartCoroutine(GetCubes(_waitTime));
     }
 
     private void Spawn()
     {
         int spawnRandom = Random.Range(0, _spawnPoints.Count);
+        int spawnRandomCube = Random.Range(0, _cubes.Count);
+
         var tempSpawnPoint = _spawnPoints[spawnRandom];
 
-        Cube cube = Instantiate(_cube);
-        cube.transform.position = tempSpawnPoint.position;
+        if (_cubes[spawnRandomCube].gameObject.activeSelf == false)
+        {
+            _cubes[spawnRandomCube].gameObject.SetActive(true);
+            _cubes[spawnRandomCube].transform.position = tempSpawnPoint.position;
+            _cubes[spawnRandomCube].ChangeLifeTimer(GetRandomTime());
+        }
+    }
+
+    private void CreateCubes(int numbersOfCubes)
+    {
+        for (int i = 0; i < numbersOfCubes; i++)
+        {
+            _cubes.Add(Instantiate(_cube));
+        }
+
+        for (int i = 0; i < _cubes.Count; i++)
+        {
+            _cubes[i].GetPlatform(_platform);
+        }
     }
 
     private IEnumerator GetCubes(float delay)
     {
         var wait = new WaitForSeconds(delay);
 
-        while (true)
+        while (enabled)
         {
             Spawn();
             yield return wait;
         }
+    }
+
+    private int GetRandomTime()
+    {
+        int minRandom = 2;
+        int maxRandom = 6;
+        int randomTime = Random.Range(minRandom, maxRandom);
+
+        return randomTime;
     }
 }
